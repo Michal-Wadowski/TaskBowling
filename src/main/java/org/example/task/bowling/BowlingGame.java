d3ae4a16fe66ed9c3f5acbc897c0df0aaabddc79
Michał Wadowski <wadosm@gmail.com>
2020-02-02 14:11:16 +0100

    Add a few first simple tests

--- src/main/java/org/example/task/bowling/BowlingGame.java
+++ src/main/java/org/example/task/bowling/BowlingGame.java
@@ -1,12 +1,17 @@
 package org.example.task.bowling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BowlingGame {
+	
+	private List<Integer> scores = new ArrayList<>();
 
 	public void roll(int pins) {
-		
+		scores.add(pins);
 	}
 	
 	public int calculateScore() {
-		return 0;
+		return scores.stream().reduce(0, (p, c) -> (p + c));
 	}
 }
--- src/test/java/org/example/task/bowling/BowlingGameTest.java
+++ src/test/java/org/example/task/bowling/BowlingGameTest.java
@@ -6,11 +6,34 @@ import org.junit.jupiter.api.Test;
 public class BowlingGameTest {
 	
 	@Test
-	public void calculateScore_ReturnZero_IfNoRolls() {
-		
+	public void calculateScore_ReturnZero_IfNoRolls() {		
 		BowlingGame bg = new BowlingGame();
 		
 		assertEquals(0, bg.calculateScore());		
+	}
+	
+	@Test
+	public void calculateScore_ReturnScore_AtFirstRollZero() {		
+		BowlingGame bg = new BowlingGame();
+		
+		bg.roll(0);
+		assertEquals(0, bg.calculateScore());		
+	}
+	
+	@Test
+	public void calculateScore_ReturnScore_AtFirstRoll() {		
+		BowlingGame bg = new BowlingGame();
+		
+		bg.roll(5);
+		assertEquals(5, bg.calculateScore());		
+	}
+	
+	@Test
+	public void calculateScore_ReturnScore_AtFirstRollTen() {		
+		BowlingGame bg = new BowlingGame();
+		
+		bg.roll(10);
+		assertEquals(10, bg.calculateScore());		
 	}
 
 }
